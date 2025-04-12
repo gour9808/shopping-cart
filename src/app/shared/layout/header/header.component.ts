6446c3af862cf3b48a8093e566b98bb7ce123695
gour9808 <gouravsharma614@gmail.com>
2025-04-12 17:12:17 +0530

    header fix

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -8,23 +8,15 @@ import { selectCartItems } from 'src/app/store/cart/cart.selectors';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  search = '';
   cartCount$: Observable<number>;
 
   constructor(private router: Router, private store: Store<AppState>) {
-    this.cartCount$ = this.store.select(selectCartItems).pipe(
-      map(items => items.length)
-    );
-  }
-
-  onSearch() {
-    console.log(this.search);
-    
-    localStorage.setItem('searchTerm', this.search);
-    this.router.navigate(['/products']);
+    this.cartCount$ = this.store
+      .select(selectCartItems)
+      .pipe(map((items) => items.length));
   }
 
   goToCart() {
